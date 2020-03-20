321bf7d86dd12ac808f3f72d0a33304f3799ffec
NinoVrolijk <60880492+NinoVrolijk@users.noreply.github.com>
2020-03-20 13:11:30 +0100

    Add files via upload

--- scripts/initializer.py
+++ scripts/initializer.py
@@ -3,24 +3,71 @@ from tree_analysis import *
 import sys
 
 class functional (Gui):
+    '''
+    A class used to adds functional properties to:
+        Tree analysis screen.
+        Central input screen.
+    Furthermore this is the main script to run the CompareMS2Gui pipeline.
+    ...
+    Parent class
+    -----------
+    Gui
+        This class creates the central input screen (non functional) of the CompareMS2Gui.
+    Attributes
+    ---------
+    X
+    Methods
+    -------
+    openFileDialog1(self):
+        Open filedialog to upload input files into text box1.
+    openFileDialog2(self):
+        Open filedialog to upload input files into text box4.
+    functional_toolbar(self)
+        Adds functional properties to the toolbar present in the central input screen of the CompareMS2Gui.
+        Multiple shortcuts for saving,loading and terminating the program are created.
+    functional_buttons(self):
+        Adds functional properties to all buttons present in the central input screen of the CompareMS2Gui
+    Quit_trigger(self)
+        Terminates program after connection of pushbutton.
+    save_trigger(self):
+        Allows the user to save input specified in the central input screen. .txt file is created.
+    load_trigger(selF)
+        Allows the user to upload previously specified inputs into to the central input screen. (Uploads .txt files)
+    new_trigger(self)
+        Wipes input specifications and return default values.
+    submitmethod(self)
+        Checks and sumbits specified input after connection of submit pushbutton. If all inputs have been approved, the program continues to the tree_analysis screen.
+        Additional pop_up screens provide the user with the sumbit status.
+    tree_processing(self)
+        After connection of the submit method, the tree analysis screen is shown. This method adds functional properties to the progress bar and present buttons.
+    return_gui(self)
+        Adds function to the return button. Allows user to go back to the central input screen and rerun the program without terminating it completely.
+    '''
     def __init__(self):
         super().__init__()
         self.functional_toolbar()
         self.functional_buttons()
 
     def openFileDialog1(self):
+        '''Open filedialog to upload input files into text box1.
+        '''
         self.filename = QFileDialog.getOpenFileName(self, 'Open file',
                                             'c:\\', "MGF file or mzML file (*.MGF *.mzML)") #Add more extensions.
-        self.filepath = self.filename[0]
-        self.text_box1.setText(self.filepath)
+        self.filepath = self.filename[0] #Filepath is present in first elemant of the retrieved filename.
+        self.text_box1.setText(self.filepath) #Set text box1 with uploaded file.
 
     def openFileDialog2(self):
+        ''' Open filedialog to upload input files into text box4.
+        '''
         self.filename = QFileDialog.getOpenFileName(self, 'Open file',
                                             'c:\\', "MGF file or mzML file (*.MGF *.mzML)") #Add more extensions.
         self.filepath = self.filename[0]
-        self.text_box4.setText(self.filepath)
+        self.text_box4.setText(self.filepath) #Set text box 4 with uploaded file.
 
     def functional_toolbar(self):
+        '''Adds functional properties to toolbar present in the main GUI screen.
+        Multiple shortcuts for saving,loading and terminating the program are created.
+        '''
         save_action = QAction('Save', self)
         save_action.setShortcut('Ctrl+S')
         new_action = QAction('New', self)
@@ -33,21 +80,27 @@ class functional (Gui):
         self.file.addAction(save_action)
         self.file.addAction(quit_action)
         self.file.addAction(new_action)
-        quit_action.triggered.connect(self.quit_trigger)  # triggers termination proces.
+        quit_action.triggered.connect(self.quit_trigger)
         new_action.triggered.connect(self.new_trigger)
         save_action.triggered.connect(self.save_trigger)
         load_action.triggered.connect(self.load_trigger)
 
     def functional_buttons(self):
+        '''Adds functional properties to all buttons present in the central input screen of the CompareMS2Gui.
+        '''
         self.cancel_button.clicked.connect(self.quit_trigger)
         self.submit_button.clicked.connect(self.submitmethod)
         self.browse_button1.clicked.connect(self.openFileDialog2)
         self.browse_button0.clicked.connect(self.openFileDialog1)
 
     def quit_trigger(self):
-        qApp.quit()
+        ''' Terminates program after connection of pushbutton.
+        '''
+        qApp.quit() #Terminates screen
 
     def save_trigger(self):
+        '''Allows the user to save input specified in the central input screen.
+        '''
         self.save_name = QFileDialog.getSaveFileName(self,'Save file',"compareMS2options","Text file(*.txt)")
         f = open(self.save_name[0], 'w')
         f.write(self.text_box1.text()+"\n")
@@ -57,12 +110,15 @@ class functional (Gui):
         f.write(self.text_box5.text()+"\n")
         f.write(self.text_box6.text()+"\n")
         f.close()
+        #To do! Add default options when the text_box is left empty. And add checkbox parameters.
 
     def load_trigger(self):
+        '''Allows the user to upload previously specified inputs into to the central input screen. (Uploads .txt file)
+        '''
         self.options_file = QFileDialog.getOpenFileName(self, 'Open file',
                                                     '', "txt file (*.txt )")
-        with open(self.options_file[0], "r") as f:
-            file = f.readlines()
+        with open(self.options_file[0], "r") as f: #Opens filename as read only.
+            file = f.readlines()                   #Read seperate lines of file.
             self.text_box1.setText(file[0])
             self.text_box2.setText(file[1])
             self.text_box3.setText(file[2])
@@ -71,6 +127,8 @@ class functional (Gui):
             self.text_box6.setText(file[5])
 
     def new_trigger(self):
+        '''Wipes input specifications and return default values.
+        '''
         self.text_box1.setText(':c')
         self.text_box2.setText('2.05')
         self.text_box3.setText('1500')
@@ -86,36 +144,42 @@ class functional (Gui):
         self.check_missing_values.setChecked(False)
 
     def submitmethod(self):
+        '''Checks and sumbits specified input after connection of submit pushbutton. If all inputs have been approved, the program continues to the tree_analysis screen.
+        Additional pop_up screens provide the user with the sumbit status.
+        '''
         msg = QMessageBox()
         msg.setWindowTitle("Submit status")
         directory_file = self.text_box1.text()
-        if len(directory_file) == 0 and len(self.filepath) > 0:
+        if len(directory_file) == 0 and len(self.filepath) > 0: #check if there is a directory file provided
             self.text_box1.setText(self.filepath)
-        fp = self.text_box1.text()
+        fp = self.text_box1.text() #Filepath is saved into variable.
         if len(fp) > len("c:") and len(self.text_box5.text()) > 1: #this determines whether a complete file path has been submitted.
             msg.setText("Congratulations, your input has been succesfully submitted. The tree is being generated")
             msg.setIcon(QMessageBox.Information)
             x = msg.exec_() #Shows message box
-            self.hide()
+            self.hide() #Hide input screen.
             self.distance_matrix_file = self.text_box5.text()
-            while True:
-                self.tree_processing(self.distance_matrix_file)
-                QtTest.QTest.qWait(10000) #Miliseconds. #cheks distance_matrix_file every X amount of miliseconds.
-
-        elif len(fp) <= len("c:") or len(self.text_box5.text()) == 0:
+            while True: #While loop used to update the tree image.
+                self.tree_processing(self.distance_matrix_file) #Creates new image each iteration of loop.
+                QtTest.QTest.qWait(10000) #Wait every X amount of miliseconds after each iteration.
+        elif len(fp) <= len("c:") or len(self.text_box5.text()) == 0: #Checks if there is a file provided.
             msg.setText("Not all inputs have been specified! Try again")
             msg.setIcon(QMessageBox.Warning)
             x = msg.exec_()  # Shows message box
             self.new_trigger() #Wipes the screen.
 
     def tree_processing(self,distance_matrix_file):
-        self.tree = tree_analysis(distance_matrix_file)
-        percentual_progress = (self.tree.upgma_result.label_count / 7) * 100  # Divide with amount of samples present in input file. 100 is generic number used to test.
+        '''Displays the tree analysis screen. Adds functionality to buttons and updates progressbar.
+        '''
+        self.tree = tree_analysis(distance_matrix_file) #Calls tree screen and passes the distance matrix file.
+        percentual_progress = (self.tree.upgma_result.label_count / 118) * 100  # Divide with amount of samples present in input file. 100 is generic number used to test.
         self.tree.progress.setValue(percentual_progress)
-        self.tree.return_button.clicked.connect(self.return_gui)
-        self.tree.show()
+        self.tree.return_button.clicked.connect(self.return_gui) #Adds functional property to return button.
+        self.tree.show() #Show tree analysis screen.
 
     def return_gui(self):
+        '''Adds function to the return button. Allows user to go back to the central input screen and rerun the program without terminating it completely.
+        '''
         self.tree.hide()
         self.show()
 
--- scripts/tree_analysis.py
+++ scripts/tree_analysis.py
@@ -6,42 +6,64 @@ from UPGMA1 import *
 
 
 class tree_analysis(QDialog):
+    '''
+    A class used to create a screen that visualizes progression in phylogenetic analysis.
+    ...
+    Attributes
+    ---------
+    distance_matrix
+        MEGA distance matrix file.
+
+    Methods
+    -------
+    image_generator(self,distance_matrix):
+        Induces UPGMA algorithm and saves newick tree format.
+    layout_image(self):
+        Creates groupbox. This vertical groupbox contains the tree image and a progress bar.
+    layout_buttons(self):
+        Create a horizontal groupbox that contains a termination and return button.
+    quit_trigger(self):
+        Terminates program after connection of pushbutton.
+    InitWindow(self):
+         Initializes window.
+    '''
     def __init__(self,distance_matrix): #add distance_matrix
         super().__init__()
-        #distance_matrix = 'first_test_fraction_gt80.meg'
-        self.title = 'ms2compare'
+        #distance_matrix = 'output3_distance_matrix_converted.meg'
+        self.title = 'compareMS2Gui'
         self.left = 600
         self.top = 250
-        self.width = 650
-        self.height = 550
+        self.width = 700
+        self.height = 650
         self.app_icon = QIcon("ms2compare.png")
         self.setWindowIcon(self.app_icon)
         self.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint)  # cancels out maximize option
         self.image_generator(distance_matrix)
 
     def image_generator(self,distance_matrix):
+        '''Calls UPGMA algorithm and saves the tree in newick format.'''
         self.upgma_result = UPGMA1(distance_matrix)
-        self.InitWindow()
+        self.InitWindow() #Call window initializer
 
     def layout_image(self):
+        '''Creates groupbox. This vertical groupbox contains the tree image and a progress bar.'''
         self.groupBox1 = QGroupBox("Tree analysis")
         self.groupBox1.setStyleSheet("QGroupBox { font-weight: bold; font-size: 18px } ")
         vbox = QVBoxLayout()
         tree_image = QLabel(self)
         tree_image.setPixmap(QPixmap('test.png'))
-        tree_image.setMaximumWidth(650)
-        tree_image.setMaximumHeight(550)
-        tree_image.setMinimumWidth(650)
-        tree_image.setMinimumHeight(550)
+        scrollArea = QScrollArea()
+        scrollArea.setWidget(tree_image)
         self.progress = QProgressBar(self)
         self.progress.setMaximum(100)
-        vbox.addWidget(tree_image)
+        vbox.addWidget(scrollArea)
         vbox.addWidget(self.progress)
         self.groupBox1.setLayout(vbox)
 
     def layout_buttons(self):
+        '''Create a horizontal groupbox that contains a termination and return button.'''
         self.button_row = QGroupBox()
-        filler_label = QLabel(self)
+        filler_label = QLabel(self) #Empty label to scale buttons.
         hbox = QHBoxLayout()
         exit_button = QPushButton('Terminate', self)
         self.return_button = QPushButton('Return', self)
@@ -63,6 +85,7 @@ class tree_analysis(QDialog):
         sys.exit()
 
     def InitWindow(self):
+        '''' Initializes window.'''
         self.setWindowTitle(self.title)
         self.setStyleSheet("background-color:white;")
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -73,10 +96,6 @@ class tree_analysis(QDialog):
         vbox.addWidget(self.button_row)
         self.setLayout(vbox)
         #self.show()
-
-
-
-
 
 #if __name__ == "__main__":
 #    App = QApplication(sys.argv)
