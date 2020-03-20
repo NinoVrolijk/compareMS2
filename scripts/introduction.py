60d7e810936f201d0414a24e0f8d87c23f8611cb
NinoVrolijk <60880492+NinoVrolijk@users.noreply.github.com>
2020-03-20 13:12:22 +0100

    Add files via upload

--- scripts/introduction.py
+++ scripts/introduction.py
@@ -4,6 +4,35 @@ from PyQt5.QtGui import *
 import sys
 
 class Introduction(QMainWindow, QWidget):
+    '''
+    A class that creates the CompareMS2Gui introduction screen. This screen relevant information about the CompareMS2 algorithm and GUI.
+    ...
+    Parent classes
+    -----------
+    QMainWindow, QWidget.
+        Both classes provide PyQT5 properties used to create the introduction screen of the CompareMS2Gui pipeline.
+
+    Attributes
+    ---------
+    X
+    Methods
+    -------
+    setimage(self)
+        Creates the image label. This image is placed in the center of the screen.
+    buttons(self)
+        A method to create the required buttons present in the CompareMS2Gui introdcution screen.
+    popup(self)
+        Reveal popup information window after connection of information button.
+    window_specs(self)
+        Additional window spefications.
+            Removed maximize button.
+            Set logo.
+    text_labels(self)
+        Method that creates all required text_labels.
+    reveal(self)
+        Reveal screen.
+
+    '''
     def __init__(self):
         super().__init__()
         self.setimage()
@@ -13,25 +42,31 @@ class Introduction(QMainWindow, QWidget):
         self.reveal()
 
     def setimage(self):
+        ''' Creates the image label. This image is placed in the center of the screen.
+        '''
         self.image = QLabel(self)
         self.image.setPixmap(QPixmap('ms2compare.png'))
         self.image.setGeometry(380,180,500,200)
 
     def buttons(self):
+        '''A method to create the required buttons present in the CompareMS2Gui introdcution screen.
+        '''
         self.continue_button = QPushButton('Continue', self)
         self.info_button = QPushButton('Information', self)
         self.continue_button.setStyleSheet(
             "border-radius: 7px;border: 2px solid ;font: bold; background-color: darkblue; color: white;border-color: white;")
         self.info_button.setStyleSheet(
-            "border-radius: 7px;border: 2px solid ;font: bold; background-color: darkblue; color: white;border-color: white;")
+            "border-radius: 7px;border: 2px solid ;font: bold; background-color: darkblue; color: white;border-color: white;") #Extensive CSS stylesheets.
         self.continue_button.resize(150,50)
         self.info_button.resize(150, 50)
         self.continue_button.move(325,450)
         self.info_button.move(475, 450)
-        self.info_button.clicked.connect(self.popup)
+        self.info_button.clicked.connect(self.popup) #Initiates popup window.
 
 
     def popup(self):
+        ''' Reveal popup information window after connection of information button.
+        '''
         info = QMessageBox()
         info.setWindowTitle("compareMS2 information")
         info.setText(" Authors,version,date,organization etc.\n Whatever information u want here.")
@@ -40,14 +75,21 @@ class Introduction(QMainWindow, QWidget):
         x = info.exec_()  # Shows message box
 
     def window_specs(self):
+        '''
+        Additional window spefications.
+            Removed maximize button.
+            Set logo.
+        '''
         self.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint) #cancels out maximize option
         self.app_icon = QIcon("ms2compare.png")
         self.setWindowIcon(self.app_icon)
-        self.setWindowTitle('ms2compare: Introduction')
+        self.setWindowTitle('compareMS2Gui')
         self.setGeometry(500, 300, 150, 150)  # Orientation of screen
         self.resize(1000, 620)  # Size of screen
 
     def text_labels(self):
+        ''' Method that creates all required text_labels.
+        '''
         self.text_header = QLabel(self)
         self.text_subtitle = QLabel(self)
         self.text_author = QLabel(self)
@@ -67,9 +109,11 @@ class Introduction(QMainWindow, QWidget):
         self.text_author.setFont(font3)
 
     def reveal(self):
+        '''Reveal screen.
+        '''
         self.show()  # show screen
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = Introduction()
-    sys.exit(app.exec_())
+#if __name__ == "__main__":
+#    app = QApplication(sys.argv)
+#    window = Introduction()
+#    sys.exit(app.exec_())
